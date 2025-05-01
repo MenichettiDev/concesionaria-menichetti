7f3a2375e222b8b272812e51c966ba91e1c95850
MenichettiDev <menichetti.dev@gmail.com>
2025-05-01 11:04:19 -0300

    feat - primera subida- creacion del mvc y mysql.data

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace concesionaria_menichetti.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
